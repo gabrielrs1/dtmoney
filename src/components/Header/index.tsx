a355bfa682f9395b45fa3342d43ce5e3610031af
Gabriel Ribeiro <gabriellribeiro208@gmail.com>
2021-05-20 10:39:44 -0300

    commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logoImg from "../../assets/logo.svg"
+import { Container, Content } from "./styles"
+
+interface HeaderProps {
+    onOpenNewTrasactionModal: () => void
+}
+
+
+export function Header({ onOpenNewTrasactionModal }: HeaderProps) {
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money" />
+                <button type="button" onClick={onOpenNewTrasactionModal}>
+                    Nova Transação
+                </button>
+            </Content>
+        </Container>
+    )
+}
